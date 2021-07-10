7f30e6b89c1d2f15421fb0127dc2633e7cf7ce36
Rolo LTV <rarguello@ltvco.com>
2021-07-09 20:12:16 -0600

    Working version

--- src/client/js/exchange.js
+++ src/client/js/exchange.js
@@ -9,9 +9,9 @@ function fetchQuotes() {
 function fetchBalances() {
   $.get( "/fetchBalances", function(data) {
     $('.js-bot-name').html('Rolo test Bot');
-    $('.js-usdt-total').html(data.USDT.total.toFixed(2));
-    $('.js-usdt-used').html(data.USDT.used.toFixed(2));
-    $('.js-usdt-avaliable').html(data.USDT.free.toFixed(2));
+    $('.js-usdt-total').html('$ ' + data.USDT.total.toFixed(2));
+    $('.js-usdt-used').html('$ ' + data.USDT.used.toFixed(2));
+    $('.js-usdt-avaliable').html('$ ' + data.USDT.free.toFixed(2));
     $('.js-btc-total').html(data.BTC.total.toFixed(8));
     $('.js-btc-used').html(data.BTC.used.toFixed(8));
     $('.js-btc-avaliable').html(data.BTC.free.toFixed(8));
@@ -21,11 +21,19 @@ function fetchBalances() {
     $('.js-xrp-total').html(data.XRP.total.toFixed(2));
     $('.js-xrp-used').html(data.XRP.used.toFixed(2));
     $('.js-xrp-avaliable').html(data.XRP.free.toFixed(2));
+  }).done(function() {
+    $.get( "/fetchQuotes", function(data) {
+      $('.js-btc-total-usdt').html('$ ' + ($('.js-btc-total').text() * data.btcPrice).toFixed(2));
+      $('.js-btc-used-usdt').html('$ ' + ($('.js-btc-used').text() * data.btcPrice).toFixed(2));
+      $('.js-btc-avaliable-usdt').html('$ ' + ($('.js-btc-avaliable').text() * data.btcPrice).toFixed(2));
+      $('.js-eth-total-usdt').html('$ ' + ($('.js-eth-total').text() * data.ethPrice).toFixed(2));
+      $('.js-eth-used-usdt').html('$ ' + ($('.js-eth-used').text() * data.ethPrice).toFixed(2));
+      $('.js-eth-avaliable-usdt').html('$ ' + ($('.js-eth-avaliable').text() * data.ethPrice).toFixed(2));
+      $('.js-xrp-total-usdt').html('$ ' + ($('.js-xrp-total').text() * data.xrpPrice).toFixed(2));
+      $('.js-xrp-used-usdt').html('$ ' + ($('.js-xrp-used').text() * data.xrpPrice).toFixed(2));
+      $('.js-xrp-avaliable-usdt').html('$ ' + ($('.js-xrp-avaliable').text() * data.xrpPrice).toFixed(2));
+    })
   });
-}
-
-function setListeners() {
-  $('.js-update-balances').on('click', fetchBalances);
 }
 
 ; (function init(){
