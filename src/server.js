26dcdb905f2b3b5ad0f599ec3dfa915cbf3335c7
Rolo LTV <rarguello@ltvco.com>
2021-07-15 22:15:54 -0600

    Interfaz de eleccion de wallets

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
+  res.sendFile(path.join(__dirname, '/client//pages/walletsRoom.html'));
 });
 app.use(express.static('src/client'));
 
